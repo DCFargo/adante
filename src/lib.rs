391379f849f6fcae662b4cd06795d6e0a81b30e8
Andrew Fargo <drew.fargo@gmail.com>
2021-01-11 18:26:31 -0600

    ArgumentType documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,64 @@
+//! # adante
+//!
+//! 'adante' is a simple library that handles the logic of user defined types
+//! in order to provide efficiency in parsing command line arguments.
+
 #[cfg(test)]
 mod tests;
 
+/// A trait describing the shared methods of both Flags and Arguments
+
 pub trait ArgumentType {
+    /// A user implemented function that takes a string as input and returns an
+    /// argument type.
+    ///
+    /// # Examples
+    /// ```
+    /// use adante::{ArgumentType, Error};
+    ///
+    /// #[derive(Debug, Clone, Copy)]
+    /// enum ErrorType {
+    ///     Syntax, // EXTREMELY simple example
+    ///             // More complex examples are shown in
+    ///             // the documentation for Error
+    /// }
+    /// impl Error for ErrorType {
+    ///     fn handle(&self) {
+    ///         ()
+    ///     }
+    ///     fn as_str(&self) -> &str {
+    ///         "Syntax Error"
+    ///     }
+    /// }
+    ///
+    ///
+    /// #[derive(Debug, Clone, Copy, PartialEq)]
+    /// enum FlagType {
+    ///     Help,
+    ///     Verbose,
+    ///     Print,
+    ///     TestFail, // NOTE: For testing only
+    ///               // Use Error
+    /// }
+    /// impl ArgumentType for FlagType {
+    ///     fn from_str<ErrorType>(key: &str, error: ErrorType)
+    ///                                -> Result<Self, ErrorType> {
+    ///         match key {
+    ///             "-h" | "--help" => Ok(Self::Help),
+    ///             "-v" | "--verbose" => Ok(Self::Verbose),
+    ///             "-p" | "--print" => Ok(Self::Print),
+    ///             _ => Err(error),
+    ///         }
+    ///     }
+    /// }
+    /// let result = match FlagType::from_str("-v", ErrorType::Syntax) {
+    ///     Ok(t) => t,
+    ///     Err(_) => FlagType::TestFail // In actual usecase this would pipe into
+    ///                                  // An Error's handle function
+    /// };
+    /// assert_eq!(result, FlagType::Verbose)
+    ///
+    /// ```
     fn from_str<E: Error>(key: &str, error: E) -> Result<Self, E>
     where
         Self: std::marker::Sized;
@@ -12,6 +69,7 @@ pub trait Error {
     fn as_str(&self) -> &str;
 }
 
+#[derive(Debug)]
 pub struct Flag<T: ArgumentType> {
     pub key: T,
     // NOTE: Thought making String generic here
@@ -20,6 +78,7 @@ pub struct Flag<T: ArgumentType> {
     pub value: Option<String>,
 }
 
+#[derive(Debug)]
 pub struct Arguments<F: ArgumentType, A: ArgumentType> {
     pub flags: Vec<Flag<F>>,
     pub actions: Vec<A>,
---
+++ target/debug/deps/adante-57d0ff887d93b142.d
@@ -0,0 +1,6 @@
+/Users/dcfargo/adante/target/debug/deps/adante-57d0ff887d93b142: src/lib.rs src/tests.rs
+
+/Users/dcfargo/adante/target/debug/deps/adante-57d0ff887d93b142.d: src/lib.rs src/tests.rs
+
+src/lib.rs:
+src/tests.rs:
