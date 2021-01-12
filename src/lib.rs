3d88b9003fbbf731ee8e29669b7d4e11bc0c5a75
Andrew Fargo <drew.fargo@gmail.com>
2021-01-11 22:38:37 -0600

    Error::as_str() documentation

--- src/lib.rs
+++ src/lib.rs
@@ -217,6 +217,36 @@ pub trait Error {
     /// test_error.handle();
     /// ```
     fn handle(&self);
+    /// A user implemented function that returns a &str (usually an error message)
+    /// depending ont he type of error it is called on.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use adante::Error;
+    ///
+    /// #[derive(Debug, Clone, Copy)]
+    /// enum ErrorType {
+    ///     Syntax,
+    ///     InvalidAction,
+    ///     InvalidFlag,
+    ///     NoFlagVal,
+    /// }
+    /// impl Error for ErrorType {
+    ///     fn handle(&self) {  }
+    ///     fn as_str(&self) -> &str {
+    ///         match self {
+    ///             Self::Syntax => "Good!",
+    ///             Self::InvalidAction => "Bad!",
+    ///             Self::InvalidFlag => "Bad!",
+    ///             Self::NoFlagVal => "Bad!",
+    ///         }
+    ///     }
+    /// }
+    ///
+    /// let test_error = ErrorType::Syntax;
+    /// assert_eq!(test_error.as_str(), "Good!");
+    /// ```
     fn as_str(&self) -> &str;
 }
 
