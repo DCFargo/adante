8e0d59f3653fccabcbb2b7a72387ec4a751a0390
Andrew Fargo <drew.fargo@gmail.com>
2021-01-11 22:56:37 -0600

    Initialize Apache 2.0 License

---
+++ target/rls/debug/deps/adante-ecd43f1ebde2b89c.d
@@ -0,0 +1,5 @@
+/Users/dcfargo/adante/target/rls/debug/deps/adante-ecd43f1ebde2b89c.rmeta: src/lib.rs
+
+/Users/dcfargo/adante/target/rls/debug/deps/adante-ecd43f1ebde2b89c.d: src/lib.rs
+
+src/lib.rs:
