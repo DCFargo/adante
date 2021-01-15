c2d58166301cba4d95ac26d649f71dcbcb3adb99
Drew Fargo <drew.fargo@gmail.com>
2021-01-15 11:21:35 -0600

    Added gitignore again? Something got desynced.

--- target/rls/debug/deps/adante-04e7e8da1ed4a27e.d
+++
@@ -1,5 +0,0 @@
-/home/dcfargo/Applications/Libraries/adante/target/rls/debug/deps/adante-04e7e8da1ed4a27e.rmeta: src/lib.rs
-
-/home/dcfargo/Applications/Libraries/adante/target/rls/debug/deps/adante-04e7e8da1ed4a27e.d: src/lib.rs
-
-src/lib.rs:
